e2a84094812259f164354467b1bec09e8d4d3f13
Sean Morton <seanmorton@hey.com>
2024-10-17 22:11:08 -0500

    refactor: one build/push script

--- build-push-tag.sh
+++ build-push-tag.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+if [ "$#" -ne 1 ]; then
+  echo "verison tag is required"
+  exit 1
+fi
+
+docker build --tag seanmorton/todo-htmx:latest --platform linux/amd64 . &&
 docker tag seanmorton/todo-htmx:latest seanmorton/todo-htmx:"$1" &&
 docker push seanmorton/todo-htmx:latest &&
 docker push seanmorton/todo-htmx:"$1" &&
--- build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker build --tag seanmorton/todo-htmx:latest --platform linux/amd64 .
